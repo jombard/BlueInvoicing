139f35ae5fe0b6b00552eb200ff567079b5c6780
Jon Lombard <jombard@hotmail.com>
2016-07-22 22:44:13 +0100

    Add project files.

---
+++ BlueInvoicer/Controllers/InvoiceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace BlueInvoicer.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public ActionResult Create()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult Settings()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BlueInvoicer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlueInvoicer.MvcApplication" Language="C#" %>
