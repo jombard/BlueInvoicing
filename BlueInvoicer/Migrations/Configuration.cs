3ee1cdc76de8e8adff68ef8d303f9cb15f271048
Jon Lombard <jombard@hotmail.com>
2016-07-23 07:51:17 +0100

    Enable migrations

---
+++ BlueInvoicer/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace BlueInvoicer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BlueInvoicer.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BlueInvoicer.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(BlueInvoicer.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ BlueInvoicer/Models/Invoice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlueInvoicer.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+    }
+}
