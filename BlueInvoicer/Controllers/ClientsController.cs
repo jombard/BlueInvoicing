aed8c4028008d97a495c9d375dc9a77ef4ec245a
Jon Lombard <jombard@hotmail.com>
2016-07-29 22:38:25 +0100

    Add create client form

---
+++ BlueInvoicer/Controllers/ClientsController.cs
@@ -0,0 +1,45 @@
+﻿using BlueInvoicer.Models;
+using BlueInvoicer.ViewModels;
+using System.Web.Mvc;
+
+namespace BlueInvoicer.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(ClientFormViewModel viewModel)
+        {
+            var client = new Client
+            {
+                ClientName = viewModel.Name,
+                Address = viewModel.Address,
+                ContactEmail = viewModel.Email,
+                InvoiceEmail = viewModel.InvoiceEmail
+            };
+
+            _context.Clients.Add(client);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Clients");
+        }
+    }
+}
---
+++ BlueInvoicer/ViewModels/ClientFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueInvoicer.ViewModels
+{
+    public class ClientFormViewModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string InvoiceEmail { get; set; }
+    }
+}
