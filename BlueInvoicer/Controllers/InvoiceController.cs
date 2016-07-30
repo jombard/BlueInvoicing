27c5152ee60087b04a62c44bf8ccf608ab4e61cb
Jon Lombard <jombard@hotmail.com>
2016-07-30 11:23:28 +0100

    Implement raise invoice page

--- BlueInvoicer/Controllers/InvoiceController.cs
+++ BlueInvoicer/Controllers/InvoiceController.cs
@@ -1,12 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using BlueInvoicer.Models;
+using BlueInvoicer.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace BlueInvoicer.Controllers
 {
     public class InvoiceController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public InvoiceController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
         public ActionResult Create()
         {
-            throw new System.NotImplementedException();
+            var viewModel = new InvoiceFormViewModel
+            {
+                Clients = _context.Clients.ToList()
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult Settings()
---
+++ BlueInvoicer/Models/InvoiceFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BlueInvoicer.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlueInvoicer.ViewModels
+{
+    public class InvoiceFormViewModel
+    {
+        public int Client { get; set; }
+
+        [Required]
+        public IEnumerable<Client> Clients { get; set; }
+    }
+}
