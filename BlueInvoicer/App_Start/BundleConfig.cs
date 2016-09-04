2aa0e35cbbd14964f276ca92172f1c2e946b1cd5
Jon Lombard <jombard@hotmail.com>
2016-09-04 21:29:12 +0100

    Show pending invoices on authorised home page

--- BlueInvoicer/App_Start/BundleConfig.cs
+++ BlueInvoicer/App_Start/BundleConfig.cs
@@ -18,12 +18,6 @@ namespace BlueInvoicer
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                "~/Scripts/jquery.form.min.js",
-                "~/Scripts/knockout-{version}.js",
-                "~/Scripts/knockout.mapping-latest.js",
-                "~/Scripts/perpetuum.knockout.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
 
--- BlueInvoicer/Controllers/HomeController.cs
+++ BlueInvoicer/Controllers/HomeController.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BlueInvoicer.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BlueInvoicer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var pendingInvoices = _context.Invoices.Where(i => !i.Paid);
+            return View(pendingInvoices);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
